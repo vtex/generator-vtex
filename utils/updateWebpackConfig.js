54233dc2b6b742058dcd326a766de61fbdb0517d
Anderson Moreira <anderson.moreira@vtex.com.br>
2016-03-06 19:54:11 -0300

    Update generator to new structure

--- utils/updateWebpackConfig.js
+++ utils/updateWebpackConfig.js
@@ -24,7 +24,7 @@ function updateEntryPoints(ast, context) {
           elements: [
             {
               type: 'Literal',
-              value: './src/pages/' + context.componentName + '/index.js'
+              value: './src/components/' + context.componentName + '/index.js'
             },
           ]
         }
